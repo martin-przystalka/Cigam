754c1b6ac31efee22bca4f537d4e700453472d4c
Martin Przystalka <>
2019-08-13 15:49:40 +0200

    add ruby script.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-exports.printMsg = function () {
-    console.log("This is a message from the demo package");
-}
+#!/usr/bin/env node
+
+const { exec } = require('child_process');
+
+exec('ruby version.rb', (err, stdout, stderr) => {
+  console.log(`${stdout}`);
+});
